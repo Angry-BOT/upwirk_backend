6bb0909e69760e051f4789e9464834360c9698b0
Rohit Halder <1705926@kiit.ac.in>
2024-04-16 01:38:32 +0530

    Search API Functionality & View API Functionality

--- src/main/java/com/upwirk/upwirk_backend/controller/CreateUserServiceController.java
+++ src/main/java/com/upwirk/upwirk_backend/controller/CreateUserServiceController.java
@@ -2,6 +2,8 @@ package com.upwirk.upwirk_backend.controller;
 
 import com.upwirk.upwirk_backend.core.Sanity;
 import com.upwirk.upwirk_backend.exceptions.UpwirdException;
+import com.upwirk.upwirk_backend.implementation.ArtistInfluencerServiceImpl;
+import com.upwirk.upwirk_backend.models.SearchCriteria;
 import com.upwirk.upwirk_backend.request.CreateUserServiceRequest;
 import com.upwirk.upwirk_backend.models.User;
 import com.upwirk.upwirk_backend.implementation.CreateUserServiceImpl;
@@ -13,18 +15,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/create")
+@RequestMapping("/api/v1/")
 public class CreateUserServiceController {
     @Autowired
     public CreateUserServiceImpl userService;
     @Autowired
     public Sanity sanity;
+    @Autowired
+    public ArtistInfluencerServiceImpl artistInfluencerService;
     Logger logger = LoggerFactory.getLogger(CreateUserServiceController.class);
     public final String SUCCESS_MSG = "User registered successfully";
     /*@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)*/
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<String> createProfile(
             @RequestBody CreateUserServiceRequest request
             /*@RequestPart(name = "images", required = false) List<MultipartFile> images*/) {
@@ -60,5 +65,40 @@ public class CreateUserServiceController {
             logger.error(e.getMessage() + Arrays.toString(e.getStackTrace()));
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
+    }
+
+    @GetMapping("/view")
+    public List<User> viewArtistInfluencers(
+            @RequestParam(required = true) String artistCategory){
+        SearchCriteria criteria = new SearchCriteria();
+        criteria.setArtistCategory(artistCategory);
+        return artistInfluencerService.viewAllArtist(criteria);
+    }
+
+    @GetMapping("/search")
+    public List<User> searchArtistInfluencers(
+            @RequestParam(required = true) String artistCategory,
+            @RequestParam(required = true) String location,
+            @RequestParam(required = true) Long minFollowers,
+            @RequestParam(required = true) Long maxFollowers,
+            @RequestParam(required = true) String pricingModel,
+            @RequestParam(required = true) Double minPrice,
+            @RequestParam(required = true) Double maxPrice,
+            @RequestParam(required = true) String availabilityStart,
+            @RequestParam(required = true) String availabilityEnd){
+
+        SearchCriteria criteria = new SearchCriteria();
+        criteria.setArtistCategory(artistCategory);
+        criteria.setLocation(location);
+        criteria.setMinFollowers(minFollowers);
+        criteria.setMaxFollowers(maxFollowers);
+        criteria.setPricingModel(pricingModel);
+        criteria.setMinPrice(minPrice);
+        criteria.setMaxPrice(maxPrice);
+        // criteria.setAvailibilityStart(availabilityStart);
+        // criteria.setAvailibilityStart(availabilityStart);
+
+        return artistInfluencerService.search(criteria);
+
     }
 }
